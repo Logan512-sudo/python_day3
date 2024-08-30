fe897ca9926fb70962bc478d04e9e286ed6438c5
Lara <lmlara1@cps.k12.il.us>
2024-08-30 09:26:04 -0500

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,46 @@
+# comments are used to explain the code
+# this is a comment it will not be executed
+# comments are notes to the programmer
+# print function allows you to print a message to the console
+print("day 3 python programming")
+# print function allows us to make an output to the console
+# input function allows you to get input from the user
+# input function will return a string
+# name = input("what is your name?")
+# print ("Hello" + name)
+# age = input("How old are you?")
+# # print the users age
+# print ("You are" + age)
+# address = input("Where do you live?")
+# #print the address of the user
+# print ("You live at" + address)
+# number = input("Can I get your digits?")
+# #print the phone number of the user
+# print ("Thanks for you for your number of" + number)
+# # name is the variable that will store the input from the user
+# # a variable is a container that stores data
+# # concentration is the process of joining string together
+# # with a avriable using the plus sign
+
+# # ask the user for their age: age = input
+# #print out the age of the user with their name
+# #ask for their home address:
+# #print out home address of the user
+# #ask fopr their phone number: 
+# #print out the phone number of the user
+
+# # ask for their email address
+# #print out their email address
+# email = input("What is your email adrdress?")
+# print ("Thank you for your email of" + email)
+
+# data types variables can store different types of data
+# string are text data -- string data type
+name = "logan" #whemever you see text in  quotes it is a string
+# Integers are whole numbers
+age = 16 #no quotes areound the number
+# Floats are decimal numbers
+height = 5.5 #a number with a decimal point
+# boolean are true or false values
+is_student = True #True or False
+# Lists are collection of data
